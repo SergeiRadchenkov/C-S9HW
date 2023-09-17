b96fed51902e019f1d0caf1d2854af186e24a753
Sergei <radchenkovsa@yandex.ru>
2023-09-17 23:02:43 +0300

    Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -6,3 +6,25 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+double SumNum(int num1, int num2)
+{
+    if (num1 == 0) num1 = 1;
+    if (num1 <= num2) return num1 + SumNum(++num1, num2);
+    else return 0;
+}
+
+
+int number1 = InputNum("Введите натуральное число значения M: ");
+int number2 = InputNum("Введите натуральное число значения N: ");
+if (number1 < 0 || number2 < 0) Console.WriteLine("Введено число не являющееся натуральным");
+else
+{
+    double result = SumNum(number1, number2);
+    Console.WriteLine(result);
+}
